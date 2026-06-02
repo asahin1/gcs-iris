276366c0d0328466dc0353ff8d20476cba6f0b52
asahin1 <als421@lehigh.edu>
2026-06-02 16:42:59 -0400

    feat: add rounding option
    - add comments
    - make GCS visualization optional

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ import cvxpy as cp
 assert "GUROBI" in cp.installed_solvers()
 from gcsopt import GraphOfConvexSets
 from gcsopt.plot_utils import plot_2d_edge
+from gcsopt.graph_problems.rounding.shortest_path import randomized_dfs
 
 X_MIN = 0
 X_MAX = 11
-
 Y_MIN = 0
 Y_MAX = 11
 
-N_SAMPLES = 10
+N_SAMPLES = 25
 
+VISUALIZE_GCS = False
+
+USE_ROUNDING = True
+NUM_PATHS = 15   # needed if USE_ROUNDING is TRUE
+MAX_TRIALS = 300 # needed if USE_ROUNDING is TRUE
 
 def ordered(poly):
     """Sort 2D vertices counterclockwise around their centroid."""
@@ -24,7 +29,9 @@ def ordered(poly):
     return poly[np.argsort(angles)]
 
 class Environment:
+    """A 2D environment representation with box-shaped obstacles."""
     def __init__(self):
+        """Initialize default environment."""
         self.domain = None
         self.obstacles = []
         self.x_min = None
@@ -33,6 +40,7 @@ class Environment:
         self.y_max = None
 
     def set_domain(self, x_min, y_min, x_max, y_max):
+        """Set domain boundaries."""
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
@@ -40,6 +48,7 @@ class Environment:
         self.domain = HPolyhedron.MakeBox((x_min, y_min), (x_max, y_max))
 
     def add_obstacle(self, x_l, y_l, x_u, y_u):
+        """Add a box-shaped obstacle."""
         self.obstacles.append(HPolyhedron.MakeBox((x_l, y_l), (x_u, y_u)))
 
     def generate_valid_sample(self, regions):
@@ -52,6 +61,9 @@ class Environment:
         return (x, y)
 
     def is_sample_valid(self, sample, regions):
+        """
+        Check if a sample is collision free and does not belong to any previous region.
+        """
         for obstacle in self.obstacles:
             if obstacle.PointInSet(sample):
                 return False
@@ -61,6 +73,7 @@ class Environment:
         return True
 
 def generate_convex_regions(environment, n_samples):
+    """Generate 'n_samples' convex regions."""
     seeds = []
     regions = []
     for i in tqdm(range(n_samples)):
@@ -73,6 +86,8 @@ def generate_convex_regions(environment, n_samples):
     return regions, seeds
 
 def main():
+
+    # Construct the environment
     environment = Environment()
     environment.set_domain(X_MIN, Y_MIN, X_MAX, Y_MAX)
     environment.add_obstacle(2, 6, 4, 8)
@@ -84,13 +99,13 @@ def main():
     directed = True
     G = GraphOfConvexSets(directed)
 
-    # Add the vertices
+    # Add the GCS vertices
     for name, region in enumerate(regions):
         v = G.add_vertex(name)
         x = v.add_variable(2)
-        v.add_constraint(region.A() @ x <= region.b())
+        v.add_constraint(region.A() @ x <= region.b()) # Vertex belongs to the region
 
-    # Insert source and target
+    # Insert source and target vertices
     source = G.add_vertex("source")
     start = source.add_variable(2)
     start_coords = (1, 1)
@@ -100,7 +115,7 @@ def main():
     goal_coords = (10, 10)
     target.add_constraint(goal == goal_coords)
 
-    # Add the edges based on intersections
+    # Add the edges based on region intersections
     intersections, _ = ComputePairwiseIntersections(regions, [])
     for v1_name, v2_name in intersections:
         v1 = G.get_vertex(v1_name)
@@ -108,9 +123,10 @@ def main():
         v1_var = v1.variables[0]
         v2_var = v2.variables[0]
         e = G.add_edge(v1, v2)
-        e.add_constraint(regions[v1_name].A() @ v2_var <= regions[v1_name].b())
+        e.add_constraint(regions[v1_name].A() @ v2_var <= regions[v1_name].b()) # Head of the edge belongs to tails region
         e.add_cost(cp.norm2(v1_var - v2_var))
 
+    # Connect source and target to the rest of GCS
     for name, region in enumerate(regions):
         v = G.get_vertex(name)
         var = v.variables[0]
@@ -121,18 +137,24 @@ def main():
             e = G.add_edge(v, target)
             e.add_cost(cp.norm2(var - goal))
 
-        
-    G.solve_shortest_path(source, target, solver=cp.GUROBI, verbose=False)
-    # Print solution statistics.
-    print("Problem status:", G.status)
-    print("Problem optimal value:", G.value)
-    for v in G.vertices:
-        x = v.variables[0]
-        print(f"Variable {v.name} optimal value:", x.value)
+    if USE_ROUNDING:
+        def rounding_fn(G, source, target):
+            return randomized_dfs(G, source, target, NUM_PATHS, MAX_TRIALS)
+        G.solve_shortest_path_with_rounding(source, target, rounding_fn, solver=cp.GUROBI)
+        print("Rounding status: ", G.status)
+        print("Rounding value: ", G.value)
+    else:
+        G.solve_shortest_path(source, target, solver=cp.GUROBI, verbose=False)
+        print("Problem status:", G.status)
+        print("Problem optimal value:", G.value)
+        for v in G.vertices:
+            x = v.variables[0]
+            print(f"Variable {v.name} optimal value:", x.value)
 
-    # Show graph using graphviz (requires graphviz).
-    dot = G.graphviz()
-    dot.view()
+    # Visualize GCS using graphviz (requires graphviz).
+    if VISUALIZE_GCS:
+        dot = G.graphviz()
+        dot.view()
 
     fig, ax = plt.subplots()
 
@@ -150,27 +172,12 @@ def main():
             head = e.head.variables[0].value
             ax.plot([tail[0], head[0]], [tail[1], head[1]],  color='blue')
 
-    # print("Vertices on GCS path: ", vertices_on_GCS_path)
-    # print("Edges on GCS path: ", edges_on_GCS_path)
-    
-    # mat = np.zeros((len(regions), len(regions)))
-    # if intersections:
-    #     rows, cols = zip(*intersections)
-    #     mat[rows, cols] = 1
-
-    # plt.imshow(mat)
-    # plt.show()
-
-    # print("Number of connected components: ", nx.number_connected_components(nx.from_numpy_array(mat)))
-
     # For visualization
     polygons = []
     for region in regions:
         poly = ordered(VPolytope(region).vertices().T)
         polygons.append(poly)
 
-    # fig, ax = plt.subplots()
-    
     labeled = False
     for obstacle in environment.obstacles:
         obst_poly = ordered(VPolytope(obstacle).vertices().T)
